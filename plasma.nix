494012c120f35c31a2104384897184a5427d9d7a
adama <1012004+socialmediarefugee@users.noreply.github.com>
2025-03-01 01:05:30 -0600

    Added plasma manager support

---
+++ plasma.nix
@@ -0,0 +1,73 @@
+{lib, pkgs, plasma-manager, ...}: {
+
+  programs.plasma = {
+    enable = true;
+
+    workspace = {
+
+      clickItemTo = "open";
+      lookAndFeel = "org.kde.breezedark.desktop";
+
+    };
+
+    fonts = {
+
+      general = {
+        family = "MonaspiceXe Nerd Font Propo";
+        pointsize = 16;
+      };
+
+    };
+
+    window-rules = [
+     {
+
+      description = "wezterm";
+
+      match = {
+        window-class = {
+          value = "wezterm";
+          type = "substring";
+        };
+        window-types = ["normal"];
+      };
+
+      apply = {
+        noborder = {
+          value = true;
+          apply = "force";
+        };
+        maximizehoriz = true;
+        maximizevert = true;
+        };
+     }
+    ];
+
+    shortcut = {
+      ksmserver = {
+        "Lock Session" = [
+          "Screensaver"
+          "Meta+Ctrl+Alt+L"
+        ];
+      }; #ksmserver
+
+      kwin = {
+        "Expose" = "Meta+,";
+        "Switch Window Down" = "Meta+J";
+        "Switch Window Left" = "Meta+H";
+        "Switch Window Right" = "Meta+L";
+        "Switch Windows Up" = "Meta+K";
+      }; # kwin
+    }; # Shortcuts
+     # This is because its a pain in the ass when accidentally hitting the touchpad.
+    configFile = {
+      "touchpadxlibinputrc" = {
+        "[Libinput][1739][52759][SYNA32A4:00 06CB:CE17 Touchpad]" = {
+          "DisableEventsOnExternalMouse" = true;
+        };
+      };
+    }; # configFile
+
+  }; # Plasma
+
+}
