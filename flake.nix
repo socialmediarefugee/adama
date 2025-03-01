07c885c0fed80d431ba552e68cbf2fcb4055e31b
adama <1012004+socialmediarefugee@users.noreply.github.com>
2025-02-28 23:39:25 -0600

    Added flake.nix.  Trying to reconcile with system.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux"; # Adjust for your system
+    in {
+      homeConfigurations."your-username" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./home.nix ];
+      };
+    };
+}
